5443921920bdd353280c8a6e3aa0f69087845748
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-27 19:52:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,6 +17,10 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
   
+  def meals
+    self.customer=[]
+  end  
+  
   # def new_meal_20_percent(waiter, total)
   #   tip = total * 0.2
   #   Meal.new(waiter, self, total, tip)
