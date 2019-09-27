82f4649c51bce16b92ed2a533ba187a0db14bbca
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-27 20:20:34 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -37,9 +37,7 @@ class Customer
     end
   end
   
-  # def new_meal(waiter, total, tip=0)
-  #   Meal.new(waiter, self, total, tip)
-  # end
+  
   
   
   # def new_meal_20_percent(waiter, total)
