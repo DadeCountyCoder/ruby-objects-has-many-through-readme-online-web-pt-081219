02c6f9a38f51ac46179d483b1e42bd2b2c37147a
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-27 19:48:26 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -22,12 +22,12 @@ class Customer
   #   Meal.new(waiter, self, total, tip)
   # end
   
-   def best_tipper
-    best_tipped_meal = meals.max do |meal_a, meal_b|
-      meal_a.tip <=> meal_b.tip
-    end
+  # def best_tipper
+  #   best_tipped_meal = meals.max do |meal_a, meal_b|
+  #     meal_a.tip <=> meal_b.tip
+  #   end
   
-    best_tipped_meal.customer
-  end
+  #   best_tipped_meal.customer
+  # end
 
 end
